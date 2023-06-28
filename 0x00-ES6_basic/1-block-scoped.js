54ad65abbc94dd7b9a7a53185be396afbce40d57
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2023-06-28 21:10:02 +0100

    tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  var task = false;
+  var task2 = true;
 
   if (trueOrFalse) {
     const task = true;
@@ -9,3 +9,8 @@ export default function taskBlock(trueOrFalse) {
 
   return [task, task2];
 }
+
+// // import taskBlock from './1-block-scoped.js';
+
+// console.log(taskBlock(true));
+// console.log(taskBlock(false));
